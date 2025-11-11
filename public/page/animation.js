3e934c84d188c9c196b287818617b49b71eb93ec
methy-tec <pmethychella@gmail.com>
2025-11-11 08:32:32 +0100

    Update animation.js

--- public/page/animation.js
+++ public/page/animation.js
@@ -14,13 +14,3 @@ function logout() {
   localStorage.clear();
   window.location.href = './login.html';
 }
-
-// Exemple : peupler les familles
-const familles = ['Famille 1', 'Famille 2', 'Famille 3'];
-const familleList = document.getElementById('familleList');
-familles.forEach(f => {
-  const div = document.createElement('div');
-  div.classList.add('list-item');
-  div.innerText = f;
-  familleList.appendChild(div);
-});
